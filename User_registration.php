6b9b9b4cc9effda4807be48b782ba94b4b2cdffe
Jhonmarl Asuncion <133129802+OONGOLDA@users.noreply.github.com>
2023-10-20 16:55:50 +0800

    Add files via upload

---
+++ User_registration.php
@@ -0,0 +1,33 @@
+<?php
+require 'user.php'; // Include the user.php file that contains the User class and database connection
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get user input from the registration form
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+
+    // Initialize the User class with the database connection
+    $user = new User($connection); // Assuming $db is your database connection
+
+    // Call the registerUser method from the User class
+    $registrationResult = $user->registerUser($username, $password, $email);
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Registration</title>
+</head>
+<body>
+    <h2>User Registration</h2>
+    <form method="post"> <!-- Removed the extra 'm' in the <form> tag and specified 'action' attribute -->
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        Email: <input type="text" name="email"><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
